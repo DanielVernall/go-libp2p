6e421cf1f9586f8c2aadb994224b41eced6c2b34
Daniel Vernall <danielv1415@hotmail.co.uk>
2023-01-01 20:42:29 +0000

    Simple AutoNAT and AutoRelay service

---
+++ examples/relay-nat-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/libp2p/go-libp2p"
+)
+
+func main() {
+	h, err := libp2p.New(
+		libp2p.EnableNATService(),
+		libp2p.EnableRelayService(),
+	)
+
+	if err != nil {
+		log.Printf("Error creating host: %v", err)
+	}
+
+	log.Printf("Listening on: %v", h.Network().ListenAddresses())
+	log.Printf("Handling protocols: %v", h.Mux().Protocols())
+}
